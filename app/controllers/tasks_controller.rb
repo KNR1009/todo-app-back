c0d1605f07ab1a1e1d828f7fcbdb04b62a0b5b9f
伊東和成 <itokazunari5375@icloud.com>
2022-01-24 09:47:30 +0900

    CURDを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,34 @@ class TasksController < ApplicationController
     tasks = Task.all
     render json: tasks
   end
+
+  def create
+    Task.create(task_params)
+    render json: {message: "新規作成しました"}
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    render json: {message: "削除しました"}
+  end
+
+
+  def show
+    task = Task.find(params[:id])
+    render json: task
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    render json: {message: "更新しました"}
+  end
+
+  private
+
+  def task_params
+    params.permit(:title, :tag)
+  end
+  
 end
