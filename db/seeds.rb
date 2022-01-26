f1522721edf884f57ae00217afe83c055a48d422
伊東和成 <itokazunari5375@icloud.com>
2022-01-26 09:14:54 +0900

    crosの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-Task.create!(
-  title: 'タスク1',
-  tag: "タグ1",
-)
+# Task.create!(
+#   title: 'タスク1',
+#   tag: "タグ1",
+# )
 
-Task.create!(
-  title: 'タスク2',
-  tag: "タグ2",
-)
-Task.create!(
-  title: 'タスク3',
-  tag: "タグ3",
-)
+# Task.create!(
+#   title: 'タスク2',
+#   tag: "タグ2",
+# )
+# Task.create!(
+#   title: 'タスク3',
+#   tag: "タグ3",
+# )
